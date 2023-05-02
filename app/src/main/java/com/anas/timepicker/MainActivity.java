4c49af66ffcba7476da8b1322357b4e74b4464d4
anas2002 <anas4112002@gmail.com>
2023-05-03 02:03:19 +0530

    Initial commit

---
+++ app/src/main/java/com/anas/timepicker/MainActivity.java
@@ -0,0 +1,28 @@
+package com.anas.timepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    TimePicker timePicker;
+    TextView txtTimeHere;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timePicker = findViewById(R.id.timePicker);
+        txtTimeHere = findViewById(R.id.txtTimeHere);
+
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                txtTimeHere.setText(hourOfDay+":"+minute);
+            }
+        });
+    }
+}
